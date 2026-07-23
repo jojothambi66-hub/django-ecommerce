17c4dd9234b108ae027a465a384687d94e3dda26
scariya45 <iamabinbinu@gmail.com>
2026-07-24 00:40:42 +0530

    Initial commit

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -121,6 +121,7 @@ LOGIN_URL="shop:userlogin"
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
